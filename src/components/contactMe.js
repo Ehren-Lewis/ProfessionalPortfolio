327c53a0ba3618bd76537f13d625cbc635fb9e57
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-11-01 19:07:25 -0500

    Added the form, added the emailjs hookup

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import emailjs from '@emailjs/browser';
 import "./contactMe.css"
 
@@ -12,8 +12,22 @@ const ContactForm =  () => {
       window.addEventListener('resize', setWidth)
     });
 
-    const onSubmit = () => {
+    const onSubmit = async e => {
         e.preventDefault();
+        // e.stopPropagation();
+
+ 
+
+        const templateParams = {
+            name: document.querySelector("#name").value,
+            email: document.querySelector("#email").value,
+            message: document.querySelector("#message").value
+        }
+
+        console.log(templateParams);
+
+        const response = await emailjs.send("service_z4n0gcu","template_747wh4q", templateParams, "4UDQw5DrveSXsqEHt");
+        
     }
 
     return (
@@ -24,24 +38,26 @@ const ContactForm =  () => {
                 <p className='text-light mx-auto project-name'>Don't be afraid to reach out!</p>
             </div>
         </div>
-        <div className='row'>
+        <div className='row mb-5'>
             <div className='col'>
                 <Form className='text-light w-50 mx-auto p-3' style={{border: "1px solid #c16632"}} id="contactForm">
                 <Form.Group className='mb-2'>
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Full Name" className='form-input'/>
+                <Form.Control id="name" type="text" placeholder="Full Name" className='form-input'/>
                 </Form.Group>
 
 
                 <Form.Group className="mb-2">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email@Address.com" className='form-input' />
+                <Form.Control id="email" type="email" placeholder="Email@Address.com" className='form-input' />
                 </Form.Group>
 
                 <Form.Group>
                 <Form.Label>Message </Form.Label>
-                <Form.Control as="textarea" row={3} className='form-input'/>
-
+                <Form.Control id="message" as="textarea" rows={3} className='form-input'/>
+                </Form.Group>
+                <Form.Group>
+                    <Button type="submit" onClick={onSubmit} className="mt-3 btn-orange">Submit</Button>
                 </Form.Group>
 
                 </Form>
