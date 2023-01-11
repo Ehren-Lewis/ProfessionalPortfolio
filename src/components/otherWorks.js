37641b36e5f2271e76c8e647474ca47e40c8eac7
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2023-01-10 21:31:38 -0600

    finsished adding the card componenets for other work. Added link, description, footer, and title

--- src/components/otherWorks.js
+++ src/components/otherWorks.js
@@ -6,62 +6,62 @@ import { faKrw } from "@fortawesome/free-solid-svg-icons";
 
 const OtherWorks = () => {
     const [windowWidth, setwindowWidth] = useState(window.innerWidth);
-    const [repoData, setRepoData] = useState();
 
     const otherWorksInfo = [
         {
             "name": "Hobby Spot",
             "link": "https://github.com/Ehren-Lewis/HobbySpot",
-            "description":"Hobby Spot",
-            "tech": "Handlebars, SQL, Sequelize"
+            "description":"Discussion site focused around allowing individuals to freely discuss Their hobbies as they please. It features a fully functional login system with User authentification, as well as using cookies to persist session information for the user.",
+            "tech": ["Handlebars", "Sequelize", "Express", "SQL"]
         },
         {
             "name": "Spirit",
-            "link": "",
-            "description": "Static web appliation focused on design and mobile responsiveness",
-            "tech": "Bootstrap, React"
+            "link": "https://github.com/Ehren-Lewis/Spirit",
+            "description": "Static web appliation focused on design and mobile responsiveness.",
+            "tech": ["Bootstrap", "React"]
         },
         {
             "name": "Deep Thoughts",
-            "link": "",
+            "link": "https://github.com/Ehren-Lewis/DeepThoughts",
             "description": " Full-stack React thought board designed to allow users to post their thoughts without having to login.",
-            "tech": "AWS, DynamoDB, React, Heroku",
+            "tech": ["AWS", "DynamoDB", "React", "Heroku"],
         },
         {
-            "name": "Trilogy Backend",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "name": "Trilogy API",
+            "link": "https://github.com/Ehren-Lewis/TrilogySocialMediaAPI",
+            "description": "Functioning backend based around MongoDB and Mongoose that allows for the creation of users, thoughts and reactions that a webstie would use as a backend.",
+            "tech": ["MongdoDB", "Mongoos", "Insomnia", "Express"],
         },
         {
             "name": "Manage Your Company",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "link": "https://github.com/Ehren-Lewis/ManageYourCompany",
+            "description": "Command line application that allows for anyone to manage a company by using persisting data techniques to view employees, roles, departments, add additional employees, role, departments, as well as update employees.",
+            "tech": ["MySql", "Inquirer", "Node.js"],
         },
         {
             "name": "Follow Your Notes",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "link": "https://github.com/Ehren-Lewis/FollowYourNotes",
+            "description": "Notes taking application. The note taker can view previously saved notes, delete a selected note one at a time, and save new notes to their liking.",
+            "tech": ["Node.js", "Insomnia", "HTML", "CSS"],
         },
         {
             "name": "FiHub",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "link": "https://github.com/Ehren-Lewis/FiHub",
+            "description": "Dynamic web application focused on bringing financial information to each user.",
+            "tech": ["Node.js", "HTML", "CSS", "APIs"],
+            "deployment": "https://ehren-lewis.github.io/FiHub/"
         },
         {
             "name": "Create Your README",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "link": "https://github.com/Ehren-Lewis/CreateYourREADME",
+            "description": "Fully automated command line interface README generator that converts your directed inputs into a fully functioning markdown readme file.",
+            "tech": ["Node.js", "Inquirer", "Readline-sync"],
         },
         {
             "name": "Dune Scraper",
-            "link": "",
-            "description": "",
-            "tech": "",
+            "link": "https://github.com/Ehren-Lewis/DuneWebScaper",
+            "description": "Python command line web scraper that gets the scores from Rotten Tomatoes, converts the scores to one common notation, and then averages the number of appearences for each score, then presents it to the user in an easy to understand format.",
+            "tech": ["Selenium", "BeautifulSoup", "MatPlotLib", "Python"],
         }  
 
     ]
@@ -109,6 +109,20 @@ const OtherWorks = () => {
                                 </div>
                             </div>
                             <p className="card-text">{ele.description}</p>
+                        </div>
+
+                        <div className="card-footer">
+                            {
+                                <ul className="technology-list">
+                                    {ele.tech.map( (tech_item) => {
+                                        return (
+                                            <li>{tech_item}</li>
+                                        )
+                                    })}
+                                </ul>
+
+                                    
+                            }
                         </div>
                     </div>
                 </div>
