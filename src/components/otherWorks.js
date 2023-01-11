e1cb7d8598e35813fec71634ee95a3c654d172a4
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2023-01-10 19:35:52 -0600

    Added more components for my other works

--- src/components/otherWorks.js
+++ src/components/otherWorks.js
@@ -1,8 +1,70 @@
 import React, { useState, useEffect } from "react";
-import {Button, Card } from "react-bootstrap";
+import {Card } from "react-bootstrap";
+import axios from 'axios';
+import "./otherworks.css"
+import { faKrw } from "@fortawesome/free-solid-svg-icons";
 
 const OtherWorks = () => {
     const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+    const [repoData, setRepoData] = useState();
+
+    const otherWorksInfo = [
+        {
+            "name": "Hobby Spot",
+            "link": "https://github.com/Ehren-Lewis/HobbySpot",
+            "description":"Hobby Spot",
+            "tech": "Handlebars, SQL, Sequelize"
+        },
+        {
+            "name": "Spirit",
+            "link": "",
+            "description": "Static web appliation focused on design and mobile responsiveness",
+            "tech": "Bootstrap, React"
+        },
+        {
+            "name": "Deep Thoughts",
+            "link": "",
+            "description": " Full-stack React thought board designed to allow users to post their thoughts without having to login.",
+            "tech": "AWS, DynamoDB, React, Heroku",
+        },
+        {
+            "name": "Trilogy Backend",
+            "link": "",
+            "description": "",
+            "tech": "",
+        },
+        {
+            "name": "Manage Your Company",
+            "link": "",
+            "description": "",
+            "tech": "",
+        },
+        {
+            "name": "Follow Your Notes",
+            "link": "",
+            "description": "",
+            "tech": "",
+        },
+        {
+            "name": "FiHub",
+            "link": "",
+            "description": "",
+            "tech": "",
+        },
+        {
+            "name": "Create Your README",
+            "link": "",
+            "description": "",
+            "tech": "",
+        },
+        {
+            "name": "Dune Scraper",
+            "link": "",
+            "description": "",
+            "tech": "",
+        }  
+
+    ]
 
     const setWidth = () => {
       setwindowWidth(window.innerWidth)
@@ -12,26 +74,114 @@ const OtherWorks = () => {
       window.addEventListener('resize', setWidth)
     })
 
+    // useEffect( () => {
+    //     const getData = () => {
+    //         const myData = axios.get(myReposUrl).then( (response) => {
+    //             setRepoData(response.data);
+    //             console.log(response.data);
+    //         });
+    //     }
+    //     getData();
+    // }, [])
+
   
+//data.url
+
     return(
         <div>
-            <div className="row">
+
+            <div style={{marginBottom:"200px"}}></div>
+            
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+            {otherWorksInfo.map( ele => {
+                
+                return (
+      
+                <div className="col" key={ele.name}>
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <div className="card-title">
+                                <div className="card-title-wrapper">
+                                    <p>{ele.name}</p>
+                                    <a href={ele.link} target={"_blank"}>
+                                        <svg width="24" className="folder" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 5h13v17h-24v-20h8l3 3zm-10-2v18h22v-15h-12.414l-3-3h-6.586z"/></svg>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className="card-text">{ele.description}</p>
+                        </div>
+                    </div>
+                </div>
+                )
+                
+            })
+            }
+                {/* <div className="col">
+                    <div className="card h-100">
+                    <div className="card-body">
+                        <div className="card-title">
+                        <div className="card-title-wrapper">
+                            <p>Hobby Spot</p>
+                            <a href="https://github.com/Ehren-Lewis/HobbySpot" target={"_blank"}>
+                            <svg width="24" className="folder" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 5h13v17h-24v-20h8l3 3zm-10-2v18h22v-15h-12.414l-3-3h-6.586z"/></svg>
+                            </a>
+                        </div>
+                        </div>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    </div>
+                </div>
+
                 <div className="col">
-                    <p className="name text-light">Other Works</p>
+                    <div className="card h-100">
+                    <div className="card-body">
+                        <div className="card-title">
+                        <div className="card-title-wrapper">
+                            <p>Title</p>
+                            <a href="https://github.com/Ehren-Lewis/HobbySpot" target={"_blank"}>
+                            <svg width="24" className="folder" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 5h13v17h-24v-20h8l3 3zm-10-2v18h22v-15h-12.414l-3-3h-6.586z"/></svg>
+                            </a>
+                        </div>
+                        </div>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    </div>
                 </div>
+
+                <div className="col">
+                    <div className="card h-100">
+                    <div className="card-body">
+                        <div className="card-title">
+                        <div className="card-title-wrapper">
+                            <p>Title</p>
+                            <a href="https://github.com/Ehren-Lewis/HobbySpot" target={"_blank"}>
+                            <svg width="24" className="folder" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 5h13v17h-24v-20h8l3 3zm-10-2v18h22v-15h-12.414l-3-3h-6.586z"/></svg>
+                            </a>
+                        </div>
+                        </div>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card h-100">
+                    <div className="card-body">
+                        <div className="card-title">
+                        <div className="card-title-wrapper">
+                            <p>Title</p>
+                            <a href="https://github.com/Ehren-Lewis/HobbySpot" target={"_blank"}>
+                            <svg width="24" className="folder" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 5h13v17h-24v-20h8l3 3zm-10-2v18h22v-15h-12.414l-3-3h-6.586z"/></svg>
+                            </a>
+                        </div>
+                        </div>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    </div>
+                </div> */}
+   
             </div>
 
-            <div className="row">
-                <div className="col-sm-6 col-4">
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>
-
-                            </Card.Title>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </div>
 
 
         </div>
--- src/layouts/header.js
+++ src/layouts/header.js
@@ -11,7 +11,7 @@ const Header = () => {
             <a className="cyber-grey" href="#about">About Me</a>
             <a className="cyber-grey" href="#projects" >Projects</a>
             <a className="cyber-grey" href="mailto:ehrenlewis0@gmail.com">Contact Me</a>
-            <a className="cyber-grey" target="_blank" onClick={ () => window.open(resume)}>Resume </a>
+            <a className="cyber-grey" target="_blank"  style={{cursor: "pointer"}} onClick={ () => window.open(resume)}>Resume </a>
         </nav>
     )
 
